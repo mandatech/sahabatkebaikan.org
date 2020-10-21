71370de8c7b8eb0b13085f9ab76d2cdb4b81e7db
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2020-10-21 14:33:50 +0700

    create register page

---
+++ src/modules/auth/register/components/RegistrationForm.js
@@ -0,0 +1,110 @@
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import FacebookIcon from 'assets/icons/facebook_icon.svg';
+import GoogleIcon from 'assets/icons/google_icon.svg';
+import DividerWithText from 'components/DividerWithText';
+import Link from 'components/Link';
+
+const useStyles = makeStyles(() => ({
+  form: {
+    padding: 8,
+    '& > *': {
+      margin: '8px 0',
+    },
+  },
+  button: {
+    flexGrow: 1,
+    // margin: 4,
+    marginTop: 8,
+  },
+}));
+
+const RegistrationForm = () => {
+  const classes = useStyles();
+
+  return (
+    <Box display="flex" flexDirection="column" mt={4}>
+      <form className={classes.form} noValidate autoComplete="off">
+        <TextField
+          id="username"
+          label="Username"
+          variant="filled"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          id="email"
+          label="Email"
+          variant="filled"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          id="phone"
+          label="No Telepon"
+          variant="filled"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          id="password"
+          label="Password"
+          variant="filled"
+          type="password"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          id="password"
+          label="Masukkan ulang Password"
+          variant="filled"
+          type="password"
+          size="small"
+          fullWidth
+        />
+        <Button variant="contained" color="secondary" fullWidth>
+          Daftar
+        </Button>
+        <DividerWithText style={{ padding: '0 32px', margin: '12px 0' }}>
+          Atau Daftar dengan
+        </DividerWithText>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          width="100%"
+          mb={2}
+          // px={1}
+        >
+          <Button
+            variant="outlined"
+            className={classes.button}
+            startIcon={<GoogleIcon style={{ width: 18, height: 18 }} />}
+            style={{ marginRight: 8 }}
+          >
+            Google
+          </Button>
+          <Button
+            variant="outlined"
+            className={classes.button}
+            startIcon={
+              <FacebookIcon
+                style={{ width: 18, height: 18, borderRadius: 2 }}
+              />
+            }
+          >
+            Facebook
+          </Button>
+        </Box>
+      </form>
+
+      <Link href="/syarat-dan-ketentuan" style={{ textAlign: 'center' }}>
+        Dengan mendaftar, kamu setuju dengan Syarat dan Ketentuan penggunaan
+        Sahabatkebaikan.org
+      </Link>
+    </Box>
+  );
+};
+
+export default RegistrationForm;
---
+++ src/modules/auth/register/screen/index.js
@@ -0,0 +1,53 @@
+import Box from '@material-ui/core/Box';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import BackIcon from '@material-ui/icons/ChevronLeft';
+import SahabatkebaikanIcon from 'assets/icons/sahabatkebaikan.svg';
+
+import RegistrationForm from '../components/RegistrationForm';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    // marginBottom: 100,
+    padding: 16,
+    paddingBottom: 32,
+    background: theme.palette.background.paper,
+  },
+  title: {
+    color: theme.palette.primary.main,
+    marginLeft: 24,
+    fontSize: 18,
+    fontWeight: 500,
+  },
+}));
+
+const RegisterScreen = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Box>
+        <Box display="flex">
+          <ButtonBase>
+            <BackIcon color="primary" />
+          </ButtonBase>
+          <Typography className={classes.title}>Daftar</Typography>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          mt={6}
+          style={{ flexGrow: 1 }}
+        >
+          <SahabatkebaikanIcon style={{ position: 'relative' }} />
+        </Box>
+      </Box>
+
+      <RegistrationForm />
+    </Box>
+  );
+};
+
+export default RegisterScreen;
