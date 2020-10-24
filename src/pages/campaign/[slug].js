e6c7f84af4af5df12e5708e1973872a6a3d21907
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2020-10-24 13:02:36 +0700

    change Header component to Higher Order Component

--- src/pages/campaign/[slug].js
+++ src/pages/campaign/[slug].js
@@ -1,3 +1,5 @@
+import { makeStyles } from '@material-ui/core/styles';
+import BackIcon from '@material-ui/icons/ChevronLeft';
 import Header from 'components/Header';
 import Layout from 'components/Layout';
 import CampaignBasicInfo from 'modules/campaign/basic-info/screen';
@@ -5,10 +7,29 @@ import DonateNowButton from 'modules/campaign/donate-now-button';
 import DonorList from 'modules/campaign/donor-list/screen';
 import CampaignStory from 'modules/campaign/story/screen';
 
+const useStyles = makeStyles(() => ({
+  headerRoot: {
+    borderBottom: 'solid 1px lightgrey',
+    // borderBottomColor: theme.palette.background.default,
+    // boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.25)',
+  },
+}));
+
 const DetailCampaign = () => {
+  const classes = useStyles();
   return (
     <Layout>
-      <Header title="Detail Campaign" />
+      <Header
+        title="Detail Campaign"
+        icon={<BackIcon />}
+        backButton={true}
+        TitleProps={{ align: 'left' }}
+        color="inherit"
+        elevation={0}
+        classes={{
+          root: classes.headerRoot,
+        }}
+      />
       <CampaignBasicInfo />
       <CampaignStory />
       <DonorList />
