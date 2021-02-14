b42cbc1cbe2c51596bacf712aeb2143b4dde0b1f
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-02-14 17:21:14 +0700

    feat: 🎸 api integration zipay user balance
    ✅ Closes: #78

--- src/modules/home/dompet/screen/index.js
+++ src/modules/home/dompet/screen/index.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import DompetIcon from 'assets/icons/dompet.svg';
 import { ZipayUserActivation } from 'modules/zipay/user-activation';
+import { checkBalance } from 'services/zipay.service';
+import formatCurrency from 'utils/formatCurrency';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,23 +31,91 @@ const useStyles = makeStyles((theme) => ({
 const Dompet = () => {
   const classes = useStyles();
   const [openZipayDialog, setOpenZipayDialog] = useState(false);
+  const [isLoading, setIsloading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  const handleCheckBalance = async () => {
+    try {
+      setIsloading(true);
+      setErrorMessage(null);
+
+      const data = await checkBalance();
+
+      setBalance(data.zipay_pocket);
+      setIsloading(false);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        setErrorMessage(error.response.data.message);
+      } else if (error.request) {
+        console.log(error.request);
+        setErrorMessage('Network error');
+      } else {
+        console.log('Error', error.message);
+        setErrorMessage(error.message);
+      }
+      setIsloading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleCheckBalance();
+  }, []);
 
   return (
     <Paper className={classes.root}>
       <DompetIcon className={classes.dompetIcon} />
       <Box display="flex" flexDirection="column">
-        <span style={{ fontWeight: 600 }}>Rp 0</span>
+        {isLoading ? (
+          <CircularProgress size={15} />
+        ) : errorMessage ? (
+          <Typography variant="caption" color="error">
+            Failed to get balance
+          </Typography>
+        ) : balance >= 0 ? (
+          <span style={{ fontWeight: 600 }}>
+            Rp {formatCurrency.format(balance)}
+          </span>
+        ) : (
+          <span style={{ fontWeight: 600 }}>Rp -</span>
+        )}
         <span style={{ fontSize: 12, color: '#7D7D7D' }}>Zipay Wallet</span>
       </Box>
-      <Button
-        className={classes.isiSaldo}
-        variant="outlined"
-        color="primary"
-        size="small"
-        onClick={() => setOpenZipayDialog(true)}
-      >
-        Aktifkan
-      </Button>
+
+      {isLoading ? (
+        <CircularProgress style={{ marginLeft: 'auto' }} size={20} />
+      ) : errorMessage ? (
+        <Button
+          className={classes.isiSaldo}
+          variant="outlined"
+          color="primary"
+          size="small"
+          onClick={() => handleCheckBalance()}
+        >
+          Reload
+        </Button>
+      ) : balance >= 0 ? (
+        <Button
+          className={classes.isiSaldo}
+          variant="outlined"
+          color="primary"
+          size="small"
+          // onClick={() => setOpenZipayDialog(true)}
+        >
+          Isi Saldo
+        </Button>
+      ) : (
+        <Button
+          className={classes.isiSaldo}
+          variant="outlined"
+          color="primary"
+          size="small"
+          onClick={() => setOpenZipayDialog(true)}
+        >
+          Aktifkan
+        </Button>
+      )}
       <ZipayUserActivation
         open={openZipayDialog}
         onClose={() => setOpenZipayDialog(false)}
