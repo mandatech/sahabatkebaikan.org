2fa8251ad3a6283f1fabd2be35720251f4e786f8
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-03-01 08:55:21 +0700

    feat: 🎸 only campaigner can view /campaign/new
    ✅ Closes: #85

--- src/pages/campaign/new/index.js
+++ src/pages/campaign/new/index.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from 'components/Layout';
 import CreateNewCampaign from 'modules/campaign/create/screen';
 import Header from 'components/Header';
 import BackIcon from '@material-ui/icons/ChevronLeft';
+import Loading from 'components/Loading';
 
 const useStyles = makeStyles(() => ({
   headerRoot: {
@@ -13,12 +15,25 @@ const useStyles = makeStyles(() => ({
 
 const CreateCampaign = () => {
   const classes = useStyles();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+    setIsLoading(true);
+    if (localStorage.getItem('token') && localStorage.getItem('data_login')) {
+      // handleValidateToken();
+      const dataLogin = JSON.parse(localStorage.getItem('data_login'));
+
+      if (dataLogin.user.role !== 'campaigner') {
+        Router.push('/');
+      }
+    } else {
+      Router.push('/');
+    }
+    setIsLoading(false);
   }, []);
 
   return (
@@ -35,6 +50,7 @@ const CreateCampaign = () => {
         }}
       />
       <CreateNewCampaign />
+      <Loading open={isLoading} />
     </Layout>
   );
 };
