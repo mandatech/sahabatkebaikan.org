e29eee126a2f62e1f2e53fa92fef6e74f7a4ddff
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-07-09 19:43:40 +0700

    feat: 🔥 change app bar on homepage
    ✅ Closes: #135

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,8 @@
-import { makeStyles } from '@material-ui/core/styles';
 import Layout from 'components/Layout';
 import Header from 'components/Header';
 import AboutScreen from 'modules/about/screen';
 
-const useStyles = makeStyles(() => ({
-  headerRoot: {
-    borderBottom: 'solid 1px lightgrey',
-  },
-}));
-
 const AboutPage = () => {
-  const classes = useStyles();
-
   return (
     <Layout
       header={
@@ -22,9 +13,6 @@ const AboutPage = () => {
           // TitleProps={{ align: 'left' }}
           // color="inherit"
           elevation={0}
-          classes={{
-            root: classes.headerRoot,
-          }}
         />
       }
     >
